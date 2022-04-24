9418ec01abe30e7c7196dbdc807344ba50656b54
Antonio <antonio@ublux.com>
2022-04-24 15:14:52 -0400

    saved changes

--- Models/contact.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Contact struct {
-	FirstName string
-	LastName  string
-}
---
+++ go-models.go
@@ -0,0 +1,10 @@
+package models
+
+type MyModel struct {
+	FirstName string
+	LastName  string
+}
+
+func Add(a, b int) int {
+	return a + b
+}
